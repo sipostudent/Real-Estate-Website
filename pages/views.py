1a3a0bd619e69fe0d33047558a52833335483762
Sipo Charles <sipocharles@yahoo.co.uk>
2020-08-22 08:06:19 +0100

    Dynamic Page Titles

--- pages/views.py
+++ pages/views.py
@@ -19,4 +19,15 @@ def index(request):
 
 
 def about(request):
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
     return render(request, 'pages/about.html', context)
