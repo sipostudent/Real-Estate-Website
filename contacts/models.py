490f3f79de884ef0b397fb4a18de8aa0a880f68d
Sipo Charles <sipocharles@yahoo.co.uk>
2020-08-25 06:02:23 +0100

    Contacts Admin Customization

--- contacts/models.py
+++ contacts/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from datetime import datetime
 
-class contact(models.Model):
+class Contact(models.Model):
   listing = models.CharField(max_length=200)
   listing_id = models.IntegerField()
   name = models.CharField(max_length=200)
-  email = models.CharField(max_length=200)
+  email = models.CharField(max_length=100)
   phone = models.CharField(max_length=100)
-  messsage = models.TextField(blank=True)
+  message = models.TextField(blank=True)
   contact_date = models.DateTimeField(default=datetime.now, blank=True)
   user_id = models.IntegerField(blank=True)
   def __str__(self):
